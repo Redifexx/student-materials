6fdfffd0a27231d3d2351600017e8aee0dac1a53
Gio <gohan1703@gmail.com>
2024-01-25 15:34:08 -0500

    EX0

--- Assignments/Ex0 - Fundamentals of AI/Source/Data Structures/LinkedList.h
+++ Assignments/Ex0 - Fundamentals of AI/Source/Data Structures/LinkedList.h
@@ -1,6 +1,42 @@
+#include <iterator>
 
 
 // LinkedList class should go in the "ufl_cap4053::fundamentals" namespace!
-namespace ufl_cap4053 { namespace fundamentals {
+namespace ufl_cap4053 {
+	namespace fundamentals {
+		template <typename T>
+		class Node
+		{
+			T data;
+			Node* front;
+			Node* back;
+		};
 
-} }  // namespace ufl_cap4053::fundamentals
+		template <typename T>
+		class Iterator {
+			T operator*() const {};
+			Iterator& operator++() {};
+			bool operator==(Iterator const& rhs) {};
+			bool operator!=(Iterator const& rhs) {};
+		};
+
+
+		template <typename T>
+		class LinkedList {
+		
+		public:
+			LinkedList<T>();
+			Iterator begin() const {};
+			Iterator end() const {};
+			bool isEmpty() const {};
+			T getFront() const {};
+			T getBack() const {};
+			void enqueue(T element) {};
+			void dequeue() {};
+			void pop() {};
+			void clear() {};
+			bool contains(T element) const {};
+			void remove(T element) {};
+		};
+	} 
+}
