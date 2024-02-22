b9d5f14ba69ee27e05a7907a4ba0cba2e6b3b5b8
Gio <44237862+Redifexx@users.noreply.github.com>
2024-02-22 01:34:26 -0500

    Update

--- Assignments/P1 - Path Planner/Source/PathSearch/PathSearch.cpp
+++ Assignments/P1 - Path Planner/Source/PathSearch/PathSearch.cpp
@@ -1,7 +1,17 @@
+#include "PathSearch.h";
+
 namespace ufl_cap4053
 {
 	namespace searches
 	{
 		// CLASS DEFINITION GOES HERE
+		PathSearch::PathSearch() {}
+		PathSearch::~PathSearch() {}
+		void PathSearch::load(TileMap* _tileMap) {}
+		void PathSearch::initialize(int startRow, int startCol, int goalRow, int goalCol) {}
+		void PathSearch::shutdown() {}
+		void PathSearch::unload() {}
+		bool PathSearch::isDone() const {}
+		std::vector<Tile* const> const getSolution() {}
 	}
 }  // close namespace ufl_cap4053::searches
--- Assignments/P1 - Path Planner/Source/PathSearch/PathSearch.h
+++ Assignments/P1 - Path Planner/Source/PathSearch/PathSearch.h
@@ -1,4 +1,7 @@
 #include "../platform.h" // This file will make exporting DLL symbols simpler for students.
+#include <vector>
+#include "../Framework/TileSystem/Tile.h"
+#include "../Framework/TileSystem/TileMap.h"
 
 namespace ufl_cap4053
 {
@@ -9,6 +12,15 @@ namespace ufl_cap4053
 		// CLASS DECLARATION GOES HERE
 			public:
 				DLLEXPORT PathSearch(); // EX: DLLEXPORT required for public methods - see platform.h
+				PathSearch();
+				~PathSearch();
+				void load(TileMap* _tileMap);
+				void initialize(int startRow, int startCol, int goalRow, int goalCol);
+				void update(long timeslice);
+				void shutdown();
+				void unload();
+				bool isDone() const;
+				std::vector<Tile* const> const getSolution() const;
 		};
 	}
 }  // close namespace ufl_cap4053::searches
