cc4707d953213a3723e61d59f243744068369ce4
Gio <44237862+Redifexx@users.noreply.github.com>
2024-02-24 22:57:00 -0500

    safety

--- Assignments/P1 - Path Planner/Source/PathSearch/PathSearch.cpp
+++ Assignments/P1 - Path Planner/Source/PathSearch/PathSearch.cpp
@@ -1,4 +1,4 @@
-#include "PathSearch.h";
+#include "PathSearch.h"
 
 namespace ufl_cap4053
 {
@@ -76,7 +76,7 @@ namespace ufl_cap4053
 					adjacentTiles.push_back(curTile);
 				}
 
-				curTile = tileMap->getTile(_r - 1, _c + 1); //LeftDown
+				curTile = tileMap->getTile(_r + 1, _c - 1); //LeftDown
 				if (curTile != nullptr)
 				{
 					adjacentTiles.push_back(curTile);
@@ -84,7 +84,7 @@ namespace ufl_cap4053
 			}
 			else
 			{
-				curTile = tileMap->getTile(_r + 1, _c - 1); //RightUp
+				curTile = tileMap->getTile(_r - 1, _c + 1); //RightUp
 				if (curTile != nullptr)
 				{
 					adjacentTiles.push_back(curTile);
@@ -102,12 +102,17 @@ namespace ufl_cap4053
 
 		// CLASS DEFINITION GOES HERE
 		PathSearch::PathSearch()
-		{}
+		{
+			searchQueue = new PriorityQueue<TileNode>(&isGreaterThan);
+		}
 		PathSearch::~PathSearch()
-		{}
+		{
+			delete searchQueue;
+		}
 
 		void PathSearch::load(TileMap* _tileMap)
 		{
+			auto startTime = std::chrono::high_resolution_clock::now();
 			tileMap = _tileMap;
 			//Loads the tiles into an unordered map that'll be treated like nodes
 			for (int r = 0; r < tileMap->getRowCount(); r++)
@@ -115,9 +120,17 @@ namespace ufl_cap4053
 				for (int c = 0; c < tileMap->getColumnCount(); c++)
 				{
 					Tile* curTile = tileMap->getTile(r, c);
-					searchGraph[curTile] = getAdjacent(r, c);
+					if (curTile->getWeight() != 0)
+					{
+						TileNode* curNode = new TileNode(curTile);
+						curNode->adjacentTiles = getAdjacent(r, c);
+						searchGraph[curTile] = curNode;
+					}
 				}
 			}
+			auto stop = std::chrono::high_resolution_clock::now();
+			auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - startTime);
+			elapsedTimes.push_back(duration.count() / 1000000.0f);
 		}
 
 		//Sets the start and goal tiles
@@ -125,8 +138,17 @@ namespace ufl_cap4053
 		{
 			tileMap->setStartTile(startRow, startCol);
 			tileMap->setGoalTile(goalRow, goalCol);
-			searchQueue.push(tileMap->getStartTile());
+			TileNode* startNode = searchGraph[tileMap->getStartTile()];
+			root = startNode;
+			root->givenCost = 0;
+			root->hCost = getDistance(root->tile, tileMap->getGoalTile());
+			root->updateFCost();
+			searchQueue->push(*startNode); //Reference
+			openTiles.insert(startNode->tile);
 			visitedTiles.insert(tileMap->getStartTile());
+			_isDone = false;
+			std::cout << "Start Tile - Row: " << startRow << ", Column:" << startCol << std::endl;
+			std::cout << "X: " << tileMap->getStartTile()->getXCoordinate() << ", Y:" << tileMap->getStartTile()->getYCoordinate() << "\n" << std::endl;
 		}
 
 		//Runs the algorithm
@@ -134,50 +156,164 @@ namespace ufl_cap4053
 		{
 			auto startTime = std::chrono::high_resolution_clock::now();
 			auto endTime = startTime + std::chrono::milliseconds(timeslice);
-			int iterations = -1;
+			int iterations = 0;
 			if (timeslice == 0)
 			{
-				iterations == 1; //Dummy int to stop the while
+				iterations = 1; //Dummy int to stop the while
 			}
-			//while (((std::chrono::high_resolution_clock::now() < endTime) || (iterations == 1)) && !searchQueue.empty())
-			if (!searchQueue.empty())
+			while (((std::chrono::high_resolution_clock::now() < endTime) || (iterations == 1)) && !searchQueue->empty())
 			{
-				Tile* curTile = searchQueue.front();
-				searchQueue.pop();
-
-				//PathPlanner Stuff
-				if (curTile == tileMap->getGoalTile())
+				Tile* curTile = searchQueue->front().tile;
+				TileNode* curNode = searchGraph[curTile];
+				auto itr = openTiles.find(curNode->tile);
+				if (itr != openTiles.end())
 				{
-					curTile->setFill(0xFF00FF00);
-					//break;
+					openTiles.erase(itr);
+				}
+				searchQueue->pop();
+
+				curNode->tile->setFill(0x7F0000FF);
+				iterations++; //to break iteration
+
+				//If Found
+				if (curNode->tile == tileMap->getGoalTile())
+				{
+					//Sets Goal Tile to Yellow
+					curNode->tile->setFill(0xFFFFFF00);
+					_isDone = true;
+					pathStack.push(curNode);
+					solution.push_back(curNode->tile);
+
+					//Gonna work back up to get path using unordered map of tiles and their parents
+					TileNode* curParent = curNode->parent;
+					
+					//Progressively adds the parent of each tile into a stack
+					while (curParent != nullptr)
+					{
+						pathStack.push(curParent);
+						solution.push_back(curParent->tile); //Adds to Overall Solution
+						curParent = curParent->parent;
+					}
+					
+					//Displays the path taken by changing color
+					while (!pathStack.empty())
+					{
+						Tile* pathTile = pathStack.top()->tile;
+						pathStack.pop();
+						if (pathStack.size() > 0)
+						{
+							pathTile->addLineTo(pathStack.top()->tile, 0xFFFF00FF);
+						}
+					}
+
+					std::cout << "Goal Found!!! :)" << std::endl;
+					std::cout << "\n";
+					std::cout << "Goal Tile - Row: " << curNode->tile->getRow() << ", Column:" << curNode->tile->getColumn() << std::endl;
+					std::cout << "X: " << curNode->tile->getXCoordinate() << ", Y:" << curNode->tile->getYCoordinate() << "" << std::endl;
+					std::cout << "\n";
+					std::cout << "Heuristic Cost: " << curNode->hCost << std::endl;
+					std::cout << "Given Cost: " << curNode->givenCost << std::endl;
+					std::cout << "Final Cost: " << curNode->fCost << std::endl;
+					std::cout << "Number of Nodes Created: " << searchGraph.size() << std::endl;
+					std::cout << "\n";
+					std::cout << "Size of Solution Path: " << solution.size() << std::endl;
+					break;
 				}
 
-
-				for (Tile* adjacent : searchGraph[curTile])
+				//Else
+				for (Tile* adjacent : curNode->adjacentTiles)
 				{
-					if (visitedTiles.find(adjacent) == visitedTiles.end())
+					if (visitedTiles.find(adjacent) == visitedTiles.end()) //if not visited
 					{
-						searchQueue.push(adjacent);
-						visitedTiles.insert(adjacent);
+						if (adjacent->getWeight() != 0) //if not inaccessible
+						{
+							searchGraph[adjacent]->givenCost = curNode->givenCost + searchGraph[adjacent]->weight;
+							searchGraph[adjacent]->hCost = getDistance(adjacent, tileMap->getGoalTile());
+							searchGraph[adjacent]->updateFCost();
+							searchQueue->push(*searchGraph[adjacent]);
+							openTiles.insert(adjacent);
+							visitedTiles.insert(adjacent);
+							searchGraph[adjacent]->parent = curNode;
+						}
+					}
+					else
+					{
+						if ((curNode->givenCost + searchGraph[adjacent]->weight) < searchGraph[adjacent]->givenCost)
+						{
+							searchGraph[adjacent]->givenCost = curNode->givenCost + searchGraph[adjacent]->weight;
+							searchGraph[adjacent]->updateFCost();
+							searchGraph[adjacent]->parent = curNode;
+							itr = openTiles.find(adjacent);
+							if (itr == openTiles.end())
+							{
+								searchQueue->push(*searchGraph[adjacent]);
+								openTiles.insert(adjacent);
+							}
+						}
 					}
 				}
-				//curTile->setFill(0x7F00FF00);
-				iterations++;
-				std::cout << "ITERATION" << std::endl;
 			}
-			std::cout << "done" << std::endl;
 		}
 
-		void PathSearch::shutdown() {}
-		void PathSearch::unload() {}
+		void PathSearch::shutdown()
+		{
+			//Clears the search queue
+			searchQueue->clear();
+
+			//Clears the Visited Set
+			visitedTiles.clear();
+
+			openTiles.clear();
+			
+			//Safety Net for Stack
+			while (!pathStack.empty())
+			{
+				pathStack.pop();
+			}
+
+			//Clears current Path
+			solution.clear();
+		}
+
+		void PathSearch::unload()
+		{
+			searchGraph.clear();
+		}
+
 		bool PathSearch::isDone() const
 		{
-			return false;
+			return _isDone;
 		}
+
 		std::vector<Tile const*> const PathSearch::getSolution() const
 		{
-			std::vector<Tile const*> tiles;
-			return tiles;
+			double totalTime = 0.0;
+			for (double time : elapsedTimes)
+			{
+				totalTime += time;
+			}
+			std::cout << "load time: " << (totalTime / elapsedTimes.size()) << std::endl;
+			return solution;
+		}
+
+		bool PathSearch::isGreaterThan(TileNode const& lhs, TileNode const& rhs)
+		{
+			//GBFS
+			//return ((lhs.hCost) >= (rhs.hCost));
+			//UCS
+			//return ((lhs.givenCost) >= (rhs.givenCost));
+			//A*
+			return ((lhs.fCost) >= (rhs.fCost));
+		}
+
+		float PathSearch::getDistance(Tile* lhs, Tile* rhs)
+		{
+			float x1 = lhs->getXCoordinate();
+			float y1 = lhs->getYCoordinate();
+			float x2 = rhs->getXCoordinate();
+			float y2 = rhs->getYCoordinate();
+			float distance = (std::pow(x2 - x1, 2)) + (std::pow(y2 - y1, 2));
+			return std::sqrt(distance);
 		}
 	}
 }  // close namespace ufl_cap4053::searches
--- Assignments/P1 - Path Planner/Source/PathSearch/PathSearch.h
+++ Assignments/P1 - Path Planner/Source/PathSearch/PathSearch.h
@@ -1,12 +1,14 @@
 #include "../platform.h" // This file will make exporting DLL symbols simpler for students.
 #include <vector>
-#include <queue> //for BFS
+#include <stack> //To Display Path
 #include <unordered_map>
 #include <unordered_set>
 #include <chrono>
 #include <iostream>
+#include <cmath>
 #include "../Framework/TileSystem/Tile.h"
 #include "../Framework/TileSystem/TileMap.h"
+#include "../PriorityQueue.h"
 
 namespace ufl_cap4053
 {
@@ -17,9 +19,63 @@ namespace ufl_cap4053
 			TileMap* tileMap;
 			bool areAdjacent(const Tile* lhs, const Tile* rhs);
 			std::vector<Tile*> getAdjacent(int _r, int _c);
-			std::unordered_map<Tile*, std::vector<Tile*>> searchGraph;
-			std::queue<Tile*> searchQueue;
+			class TileNode 
+			{
+			public:
+				Tile* tile;
+				TileNode* parent;
+				float givenCost;
+				float hCost;
+				float hWeight;
+				float fCost;
+				std::vector<Tile*> adjacentTiles;
+				unsigned char weight;
+				TileNode()
+				{
+					tile = nullptr;
+					weight = 0;
+					hWeight = 0.5f;
+					parent = nullptr;
+					givenCost = 0;
+					hCost = 0;
+					fCost = 0;
+				}
+				TileNode(Tile* _tile)
+				{
+					tile = _tile;
+					weight = _tile->getWeight();
+					hWeight = 0.5f;
+					parent = nullptr;
+					givenCost = 0;
+					hCost = 0;
+					fCost = 0;
+				}
+				void updateFCost()
+				{
+					fCost = givenCost + (hWeight * hCost);
+				}
+			};
+			TileNode* root;
+
+			std::unordered_map<Tile*, TileNode*> searchGraph;
+
+			PriorityQueue<TileNode>* searchQueue;
+
+
+			std::vector<double> elapsedTimes;
+
+			std::unordered_set<Tile*> openTiles;
 			std::unordered_set<Tile*> visitedTiles;
+
+			bool _isDone;
+
+			std::stack<TileNode*> pathStack;
+
+			std::vector<Tile const*> solution;
+
+			static bool isGreaterThan(TileNode const& lhs, TileNode const& rhs);
+			float getDistance(Tile* lhs, Tile* rhs);
+
 			// CLASS DECLARATION GOES HERE
 			public:
 				DLLEXPORT PathSearch(); // EX: DLLEXPORT required for public methods - see platform.h
