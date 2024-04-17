3423c9665f8351975e3f109d4719951631cb47d4
Gio <44237862+Redifexx@users.noreply.github.com>
2024-04-17 14:16:36 -0400

    SETUP DONE

---
+++ Assignments/P2 - Robocode/Source/StudentRobot/Redibot.cs
@@ -0,0 +1,26 @@
+﻿using Robocode;
+using System;
+
+namespace CAP4053.Student
+{
+    public class RediBot : TeamRobot
+    {
+        override public void Run()
+        {
+            Console.WriteLine("RUNNING");
+            while (true)
+            {
+                Ahead(100);
+                TurnRight(90);
+                Back(100);
+                TurnGunRight(360);
+            }
+        }
+
+        override public void OnScannedRobot(ScannedRobotEvent e)
+        {
+            Fire(1);
+        }
+
+    }
+}
